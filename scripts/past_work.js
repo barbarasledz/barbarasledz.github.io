633ab8111d880f04508fb3e64184f9d163c3534d
Barbara Sledz <BarbaraSledz2022@u.northwestern.edu>
2022-07-20 10:43:02 -0500

    Added some information on past work, updated some flexbox properties of cards on that page

--- scripts/past_work.js
+++ scripts/past_work.js
@@ -24,24 +24,17 @@ const cards_info = [
         desc: [""]
     },
     {
-        name: "d",
-        type: "",
-        date: "",
-        image: "",
-        alt_text: "",
-        desc: [""]
-    },
-    {
-        name: "e",
-        type: "",
-        date: "",
-        image: "",
-        alt_text: "",
-        desc: [""]
+        name: "Piano Forte",
+        type: "Group Design Project",
+        date: "Spring 2019",
+        image: "images/piano-forte.png",
+        alt_text: "Photo of a special seat created for use by a pianist; it features a flattened bottom portion of an office chair and a small, rectangular, adjustable backrest.",
+        desc: ["The \"Piano Forte\" is a seat designed for a pianist in order to help increase comfort and alleviate pain related to playing the piano for long periods of time.",
+        "The team was inspired by the adjustable designs of office chairs (compared to the common non-adjustable designs of piano benches) and created a product that offered support for the pianist's posture and accomodated the pianist's need for mobility, such as when leaning to play in different registers."]
     },
     {
         name: "The DAVL",
-        type: "Project",
+        type: "Group Design Project",
         date: "Fall 2018",
         image: "images/davl-poster.png",
         alt_text: "Image of poster showcasing the main advantages of the DAVL design",
@@ -49,5 +42,15 @@ const cards_info = [
             "The DAVL, or \"Daily Accessory for Vibrant Lifestyles\", is a product designed for patients with Heartware LVAD devices who are looking for a more suitable carrying method for their device.", 
             "This project was created as part of Northwestern's Design, Thinking, and Communication (DTC) class alongside other students."
         ]
-    }
+    },
+    {
+        name: "Student Tech Crew Member",
+        type: "Work Experience",
+        date: "Summer 2015 to Spring 2018 (Leader: Summer 2017 to Spring 2019)",
+        image: "",
+        alt_text: "",
+        desc: ["Starting at the end of freshman year in high school, I began to get involved in the student tech crew at Northside College Preparatory High School, under the coordination of Edin Ramovic, the school's tech coordinator.",
+        "My responsibilities included helping maintain, fix, or replace existing technology around the school as well as helping manage the distribution, maintenance, and collection of over a thousand Chromebook devices to the student body.",
+        "At the start of my junior year, I became the student leader of the NCP tech crew, and remained its leader until my graduation from high school in June 2018."]
+    },
 ]
--- scripts/vue_cards.js
+++ scripts/vue_cards.js
@@ -1,10 +1,11 @@
 Vue.component("card-item", {
     template: `
     <div class="card">
-        <img class="card_img" :src="image" :alt="alt_text">
         <h2>{{name}}</h2>
         <h3>{{type}}, {{date}}</h3>
         <p v-for="par in desc">{{par}}</p>
+        <br v-if="image.length>0">
+        <img v-if="image.length>0" class="card_img" :src="image" :alt="alt_text">
     </div>
     `,
     props: ["name", "desc", "image", "date", "type", "alt_text"]
