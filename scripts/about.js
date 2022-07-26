b693ac6741988a4983db33831b9869e9e370d13d
Barbara Sledz <BarbaraSledz2022@u.northwestern.edu>
2022-07-26 13:23:00 -0500

    Added the about page, fixed some styles to increase readibility on pages with large amounts of text

---
+++ scripts/about.js
@@ -0,0 +1,101 @@
+/* I'm using javascript for this for the sake of convenience; I hate having to write 5 bajillion paragraph tags around stuff */
+const life_story = [
+    {
+        time: "Early Childhood",
+        text: [
+            "Upon meeting me, some people may be surprised to learn that English is not my first language. In fact, I wasn't born in the US, either.",
+            "I was born in a small and dreary town in Southern Poland. I spent much of my early childhood in the town my parents are from, going to preschool and seeing my family often and just being a kid. My mom took care of me most of the time, since my dad was often away.",
+            "The move to the US was sudden and surprising. My family applied for the green card lottery \"because why not\" and unexpectedly got selected. A few months later, we were on a plane and headed to the US.",
+            "Back then, six-year-old me didn't know how much everything would change."
+        ],
+        image: "images/poland_map.png",
+        image_alt: "A map of Poland with a marker on the southern portion, north of Tarnow, indicating the approximate location of where my family is from.",
+        image_caption: "The approximate location of my hometown in Poland. Image from Free Vector Maps, modified. <a href=\"https://freevectormaps.com/poland/PL-EPS-01-0002?ref=atr\">Attribution</a>."
+    },
+    {
+        time: "Starting in the US",
+        text: [
+            "The first few years in the US were difficult. My parents and I knew barely any English -- I had learned my first two words in English, \"hello\" and \"goodnight\", on the plane to the US.",
+            "We ended up in Chicago, which was a common destination for Polish people moving to America. We moved into a small apartment; the only furnishing we really had on that first day was an air mattress we had bought or loaned from someone.",
+            "Barely a few days later, I was enrolled in the local public elementary school for kindergarten. I had been really lucky -- the kindergarten teacher was Polish. I don't know how I would have gotten through that first year at all without her.",
+            "The next few years of my life were rough. School was incredibly difficult, and I could not rely on my parents being able to help me with reading or history homework in English. Early on, there were moments when I was at risk of failing.",
+            "When I was about to start 4th grade, things were starting to improve for me in school -- and then a massive change came along. My family ended up moving out of the apartment and to a house in another part of the city (which also meant changing schools and likely losing contact with the friends I had back then). It was our first-ever house in the US and full of problems, ranging from flooding to fire hazards, but it was ours, with no one living above us and tap dancing at 3 in the morning.",
+            "It was around 6th grade that I finally started doing better in school, where I felt like I wasn't drowning in confusion anymore. It was motivating. In seventh grade, I got straight-As for probably the first time, and during eighth grade, I not only got really good grades but also got accepted into one of the top selective enrollment schools in the city -- Northside College Prep."
+        ],
+        image: "",
+        image_alt: "",
+        image_caption: ""
+    },
+    {
+        time: "High School and Life Goals",
+        text: [
+            "Then came high school. As a selective enrollment school, it was full of people who were also the best from their elementary and middle schools. Many people also came from academic centers, where they already got a taste of the high school life. I suddenly felt a little bit like my younger self again, surrounded by people who were more experienced and better prepared than me. Except this time, the challenge didn't feel stiffling or drowning. It was welcome.",
+            "It was by chance that I ended up taking an introductory computer science course during my freshman year of high school.",
+            "This led to me taking as many CS classes as I could throughout high school. I enjoyed every single one of them.",
+            "It was sometime in junior year that I started to get an idea of what I might want to do in life, but even then, I wasn't certain. Sure, I enjoyed the CS classes I'd been taking thus far, but I was also an outsider because of it. In my AP CS class, I was one of three girls in a class of 32. My friends circles were split between the girls I hung out with during lunch and the guys I hung out with during and after class.",
+            "Over time, I learned not to focus on that as much. I stopped worrying that I was a minority and instead focused on improving myself to be better, to be the best. And that hard work did pay off -- in my senior year, I was named the recepient of the Excellence in Computer Science award for my graduation year; a plaque with the award and my name still hangs in the main hallway of my high school."
+        ],
+        image: "images/ncp_googlemaps.png",
+        image_alt: "Google Streetview photo of Northside Colleg Prep, the high school I went to.",
+        image_caption: "A photo of Northside College Prep, the high school I attended. Sourced from Google Maps."
+    }, 
+    {
+        time: "College",
+        text: [
+            "For college, I ended up attending Northwestern University. I'll admit, it wasn't the place I was expecting to end up at -- I applied there without expecting to be accepted, mainly doing so after being convinced by those around me to try. After all, I didn't really have much to lose by sending in an application.",
+            "As you can imagine, not even I expected to get back a response of \"YES!\"",
+            "The transition to college was tough, but not bad. I quickly discovered more about myself than ever before. I made new friends and met amazing professors and TAs, some of whom inspired me to apply to be a peer mentor and help other students myself. It was like a dream come true; by sophomore year, I was already looking into applying for internships or even volunteer work for the summer.",
+            "Then came the unexpected trial that no one expected: the COVID-19 pandemic. Classes went online, the state went into lockdown, and everything felt like it was falling apart.",
+            "I wish I could say I recovered quickly and turned the hardships of the pandemic into opportunity, but I didn't. The pandemic had hit me hard, coming at an already difficult time in my personal life. This was only further compounded by a medical scare of my own (and worry that I won't be able to get medical help during the lockdown, with everyone so focused on the pandemic). School didn't offer much escape from this, either. Online classes felt soulless; lectures felt bland, few of my classmates participated in discussions, and everyone was to some degree distrated with thinking about the virus. All in all, half of my sophomore year and the entirety of my junior year felt like it had gone to waste.",
+            "Except not completely.",
+            "As a peer mentor, I ended up assigned to help an introductory CS class for non-CS majors in the spring of 2020, when COVID-19 had first hit. This ended up being the silver lining of the pandemic -- it gave me something to focus on. I wanted the students in the class to get a good experience out of their first and potentially only CS course. Though leading tutorials was difficult and sometimes felt like I was talking to a void of names on a screen, my efforts were rewarded as the various students I worked with did well despite the circumstances.",
+            "I continued peer mentoring throughout the rest of my time at Northwestern after that, often working with students who had never written a single line of code in their life and helping them learn a valuable life skill by the end of ten short weeks.",
+            "Eventually, in-person classes began to resume to some degree by Fall 2021, at the start of my final year. I went through my final undergraduate classes with a greater appreciation for everything and everyone around me, trying to make the best of the opportunity I was given to reconnect with my peers in-person one more time before graduation. I felt revitalized by the greater challenges instructors were willing to give during in-person class sessions and the increased energy that everyone seemed to have and feel. Despite the pandemic still lingering in the background, it felt like we finally claimed back the experiences that had been taken from us."
+        ],
+        image: "",
+        image_alt: "",
+        image_caption: ""
+    }, 
+    {
+        time: "The Present and the Future",
+        text: [
+            "Graduating from Northwestern was a whirlwind of activity, but eventually, it was over, and a new chapter began.",
+            "After graduation, I took a small break and went on a trip to Europe to visit my family in Poland. I wanted to thank my family for how much they had backed me over the years.",
+            "Then, upon returning home, I began to work on some of my personal projects -- and on job applications.",
+            "And that leads us to today.",
+            "Who knows what's next? I sure don't, but I'm determined to find out and make the future the best that it can be."
+        ],
+        image: "",
+        image_alt: "",
+        image_caption: ""
+    }
+];
+
+
+Vue.component("life-story-part", {
+    template: `
+    <div>
+        <h2>{{time}}</h2>
+        <div class="image_cap_container" v-if="image.length>0" >
+            <img :src="image" :alt="alt_text" class="img_in_container">
+            <p v-html="img_caption" class=img_caption_in_container></p>
+        </div>
+        <p v-for="par in text">{{par}}</p>
+    </div>
+    `, // add <div style="clear: both;"></div> maybe?
+    props: ["time", "text", "image", "img_caption", "alt_text"]
+})
+
+new Vue({
+    el: "#content_container", 
+    template: `
+    <main> 
+        <h1>My Life: The Long Story</h1>
+        <life-story-part v-for="life_part in life_story" :key="life_part.time" :time="life_part.time" :text="life_part.text" :image="life_part.image" :img_caption="life_part.image_caption" :alt_text="life_part.image_alt">
+        </life-story-part>
+    </main>
+    `,
+    data: {
+        life_story: life_story
+    }
+})
