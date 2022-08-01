dd0d4f3cea044fd57959ab78c152c9495df14eb6
Barbara Sledz <BarbaraSledz2022@u.northwestern.edu>
2022-08-01 12:07:57 -0500

    updated readme, modified page content to improve wording

--- readme.md
+++ readme.md
@@ -10,9 +10,12 @@ The live deployment of the website is available at: [https://barbarasledz.github
 This website is made primarily using HTML/CSS/JS, alongside with the Vue framework. Additional technologies may be added as the website is expanded upon to meet new needs. 
 
 
-## Schedule
+## Schedule and Changelog
 - 16 June 2022 -- Creation of repository, start of design planning using low-fidelity mockups on paper. Small skeleton of an initial layout created. Added placeholder content and began setup of multiple pages.
 - 17 June 2022 -- Work on past projects page, initial styling of info cards. Added functionality to dynamically fill in values for the cards using Vue 2.6; may update this in the future to use Vue 3 instead (Vue 2.6 is what I'm more familiar with, but Vue 3 is probably better).
+- Late July 2022 -- Finished adding additional pages for about section, past work, and current work. 
+- 01 August 2022 -- Small updates to website content.
+- (Planned) August 2022 -- CSS overhaul to mobile-first, improvements in clarity
 
 ## Credits
  - [Color palette inspired by coloors tool](https://coolors.co/e7ecef-6096ba-a3cef1-272635-001011) 
--- scripts/past_work.js
+++ scripts/past_work.js
@@ -6,9 +6,9 @@ const cards_info = [
         image: "",
         alt_text: "",
         desc: [
-            "For a total of 8 of my 12 undergraduate quarters of classes at Northwestern University, I worked as a peer mentor for the Computer Science department in the McCormick School of Engineering. Through this program, I was tasked with aiding instructors in teaching their courses.",
-            "Most of my responsibilities involved direct contact with the students who were taking these classes, from leading tutorials and lab sessions to hosting office hours to help students with their projects and studies. During the COVID-19 pandemic, especially when classes were online, these tasks became even more vital in helping ensure that students were not at risk of falling behind.",
-            "Other responsibilities included grading, providing feedback on course changes across quarters, and (in later quarters) helping train newer peer mentors."
+            "Starting from my sophomore year at Northwestern University, I worked as a peer mentor for the Computer Science department in the McCormick School of Engineering.",
+            "Most of my responsibilities involved direct contact with the students, from leading tutorials and lab sessions to hosting office hours to help students with their projects and studies. During the COVID-19 pandemic, especially when classes were online, these tasks became more vital than ever.",
+            "Other responsibilities included grading, providing feedback to instructors on course changes across quarters, and (in later quarters) helping train newer peer mentors."
         ]
     },
     {
@@ -18,8 +18,9 @@ const cards_info = [
         image: "images/classical-streaming-app-design.png",
         alt_text: "Example screenshots of a mockup of an app that's designed for listening to classical music.",
         desc: [
-            "Spotify and its music streaming rivals have a search and organization system that works well for most types of popular music. However, a process of needfinding interviews done by my team discovered that these popular streaming apps fail in one genre that does not align to the common title-album-artist model: classical music.",
-            "In response, the team began work on an app tailored for meeting the needs of listeners of classical music. Through agile development and weekly sprints, we tested over a dozen prototypes (ranging from low-fi paper mockups to higher-fidelity wireframes) in the span of a few weeks until we discovered a search system that was intuitive, simple, and efficient for users. At the end of the process, we did a final defense of our design and process before our classmates and professors."
+            "An app (prototype) designed for listeners of classical music.",
+            "Spotify and its music streaming rivals have a search and organization system that works well for most types of popular music. However, needfinding interviews done by my team discovered that these popular streaming apps fail in one genre that does not align to the common title-album-artist model: classical music.",
+            "Through agile development and weekly sprints, we tested over a dozen prototypes (ranging from low-fi paper mockups to higher-fidelity wireframes) in the span of a few weeks until we discovered a search system that was intuitive, simple, and efficient for users. At the end of the process, we did a final defense of our design and process before our classmates and professors."
         ]
     },
     {
@@ -29,8 +30,8 @@ const cards_info = [
         image: "",
         alt_text: "",
         desc: [
-            "\"MMORPGs: The Tech and the Compromises\" is an article series that looks at some of the design decisions and compromises that must be made when creating a game with lots of content and lots of simultaneous players. It explains why MMORPGs all seem to have similar inherent flaws, why those flaws exist, and what alterative design solutions exist. The examples in the articles feature many popular games, from the often-studied game \"World of Warcraft\" to the technologically-interesting systems of \"Elder Scrolls Online\".",
-            "It is meant for players of MMORPGs of any level. This target audience is likely familiar with some of the common player jargon, such as \"patch day\" or \"maintenance\" or \"instance\". However, they may not be familiar with more technical terms and acronyms like \"TCP\" and \"UDP\" and what the difference is between the two."
+            "\"MMORPGs: The Tech and the Compromises\" is an article series that looks at some of the design decisions and compromises that must be made when creating an MMORPG game. It explains why MMORPGs all seem to have similar inherent flaws, why those flaws exist, and what alterative design solutions exist. The examples in the articles feature many popular games, from the often-studied game \"World of Warcraft\" to the technologically-interesting systems of \"Elder Scrolls Online\".",
+            "The series was written with MMORPG players of any level in mind. This target audience is likely familiar with some of the common player jargon, such as \"patch day\" or \"maintenance\" or \"instance\". However, they may not be familiar with more technical terms and acronyms like \"TCP\" and \"UDP\" and what the difference is between the two."
         ]
     },
     {
@@ -41,7 +42,7 @@ const cards_info = [
         alt_text: "Photo of a special seat created for use by a pianist; it features a flattened bottom portion of an office chair and a small, rectangular, adjustable backrest.",
         desc: [
             "The \"Piano Forte\" is a seat designed for pianist Ms. Gabriela Calderon-Cornejo in order to help increase comfort and alleviate pain related to playing the piano for long periods of time.",
-            "The team was inspired by the adjustable designs of office chairs (compared to the common non-adjustable designs of piano benches) and created a product that offered support for the pianist's posture and accomodated the pianist's need for mobility, such as when leaning to play in different registers.",
+            "The team created a prototype that offers support for the pianist's posture and accomodates the pianist's need for mobility, such as when leaning to play in different registers.",
             "This project was created as part of Northwestern's Design, Thinking, and Communication (DTC) class alongside other students."]
     },
     {
@@ -51,7 +52,7 @@ const cards_info = [
         image: "images/davl-poster.png",
         alt_text: "Image of poster showcasing the main advantages of the DAVL design",
         desc: [
-            "The DAVL, or \"Daily Accessory for Vibrant Lifestyles\", is a product designed for patients with Heartware LVAD devices who are looking for a more suitable carrying method for their device.", 
+            "The DAVL, or \"Daily Accessory for Vibrant Lifestyles\", is a product designed for patients with Heartware LVAD devices who are looking for a more suitable carrying method for their device. The team prioritized both comfort and safety when creating the design.", 
             "This project was created as part of Northwestern's Design, Thinking, and Communication (DTC) class alongside other students."
         ]
     },
@@ -61,9 +62,10 @@ const cards_info = [
         date: "Summer 2015 to Spring 2018 (Leader: Summer 2016 to Spring 2018)",
         image: "",
         alt_text: "",
-        desc: ["Starting at the end of freshman year in high school, I began to get involved in the student tech crew at Northside College Preparatory High School, under the coordination of Edin Ramovic, the school's tech coordinator.",
+        desc: [
+            "Starting at the end of freshman year in high school, I began to get involved in the student tech crew at Northside College Preparatory High School, under the coordination of Edin Ramovic, the school's tech coordinator.",
             "My responsibilities included helping maintain, fix, or replace existing technology around the school as well as helping manage the distribution, maintenance, and collection of over a thousand Chromebook devices to the student body.",
-            "In order to complete these responsibilities while being a student, I had to arrive well before classes started in the mornings; days with more work required staying for long hours after school ended. Sometimes it felt like the only people in the building were the janitors, the security, and tech crew.",
-        "At the start of my junior year, I became the student leader of the NCP tech crew, and remained its leader until my graduation from high school in June 2018."]
+            "At the start of my junior year, I became the student leader of the NCP tech crew, and remained its leader until my graduation from high school in June 2018."
+        ]
     },
 ]
